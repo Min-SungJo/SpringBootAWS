7c447458f246a4e4d5877527773c263f3cd5d58f
Min-SungJo <123or@naver.com>
2023-02-25 18:17:39 +0900

    HelloController 에 HelloResponseDto 생성(lombok 적용), Test 코드생성 및 확인

--- src/main/java/com/sj/study/web/HelloController.java
+++ src/main/java/com/sj/study/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.sj.study.web;
 
+import com.sj.study.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 컨트롤러가 JSON 을 반환하게 만들어줌
@@ -9,5 +11,12 @@ public class HelloController {
     @GetMapping("/hello") // HTTP Method Get 을 받을 수 있는 API 를 만들어줌
     public String hello() {
         return "hello";
+    }
+
+    //@RequestParam -> 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션
+    //외부 에서 name 이라는 이름으로 넘긴 파라미터를 메소드 파라미터 String name 에 저장
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name,amount);
     }
 }
--- src/test/java/com/sj/study/web/HelloControllerTest.java
+++ src/test/java/com/sj/study/web/HelloControllerTest.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnit 에 내장된 실행자 외에 다른 실행자를 실행시킴, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함
 @WebMvcTest(controllers = HelloController.class) // 현재 클래스에서 컨트롤러만 사용할 경우 선언, @Controller, @ControllerAdvice 사용 가능
@@ -28,6 +27,18 @@ public class HelloControllerTest {
         mockMvc.perform(get("/hello")) // 다음 주소로 HTTP GET 요청, 체이닝 지원 -> 여러 검증을 이어서 할 수 있음
                 .andExpect(status().isOk()) // 200, 404, 500 등 HTTP Header Status 검증
                 .andExpect(content().string(hello)); // 리턴값(content)이 맞는지 검증
+    }
+
+    @Test
+    public void testHelloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+        mockMvc.perform(get("/hello/dto")
+                .param("name", name) // API 테스트할 때 사용될 요청 파라미터를 설정, 단 값은 String 만 허용(숫자, 날짜도 문자열로)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메소드, $ 를 기준으로 필드명 명시
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 }
