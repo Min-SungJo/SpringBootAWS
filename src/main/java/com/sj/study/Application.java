6f458f4e57fce523ccba2340cb80c8983d92bcf0
Min-SungJo <123or@naver.com>
2023-02-25 16:34:16 +0900

    프로젝트 최상단에 @SpringBootApplication 추가

---
+++ src/main/java/com/sj/study/Application.java
@@ -0,0 +1,13 @@
+package com.sj.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**@SpringBootApplication 으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정.
+ * 작성 위치 부터 설정을 읽기 때문에 프로젝트 최상단에 위치해야함 */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // 위 코드를 통해 내장 WAS 를 실행함 -> 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있음
+    }
+}
