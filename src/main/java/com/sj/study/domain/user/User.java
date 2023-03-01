472891f64df518772304b4076e5f08754a19327b
Min-SungJo <123or@naver.com>
2023-03-01 22:51:46 +0900

    사용자 정보를 담을 User 클래스와 권한을 관리할 Role 클래스 주석 추가 User CRUD 용 UserRepository 인터페이스 생성

--- src/main/java/com/sj/study/domain/user/User.java
+++ src/main/java/com/sj/study/domain/user/User.java
@@ -24,6 +24,8 @@ public class User extends BaseTimeEntity {
     @Column
     private String picture;
 
+    // JPA 로 데이터베이스에 값을 저장할 때, Enum 값을 어떤 형태로 저장할지를 결정함
+    // (기본적으로 int 이나 숫자가 의미하는 값을 알 수 없기 때문에 이를 문자열로 저장할 수 있도록 선언)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
---
+++ src/main/java/com/sj/study/domain/user/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sj.study.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * User 의 CRUD 를 책임짐
+ * */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**소셜 로그인으로 반환되는 값 중 email 을 통해 이미 생성된 사용자인지 처음 가입하는 사용자인지 판단하기 위한 메소드*/
+    Optional<User> findByEmail(String email);
+}
