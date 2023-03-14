c2151fd48fd9644bc1a9f751f4b11d5b0c0797bf
Min-SungJo <123or@naver.com>
2023-03-14 16:37:22 +0900

    OAuthAttributes 에 네이버인지 판단하는 코드 & 생성자 추가

--- src/main/java/com/sj/study/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sj/study/config/auth/dto/OAuthAttributes.java
@@ -32,14 +32,32 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                       String userNameAttributeName,
                                       Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
+
+    /** 구글인지 판단하는 생성자 */
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    /** 네이버인지 판단하는 생성자 */
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
