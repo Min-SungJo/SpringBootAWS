213857a89eb7dfae854d9042efe9e6b1ff6a0580
Min-SungJo <123or@naver.com>
2023-03-14 15:39:48 +0900

    indexController 에서 세션값을 가져오는 부분 반복하지 않게 적용

--- src/main/java/com/sj/study/web/IndexController.java
+++ src/main/java/com/sj/study/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sj.study.web;
 
+import com.sj.study.config.auth.LoginUser;
 import com.sj.study.config.auth.dto.SessionUser;
 import com.sj.study.service.posts.PostsService;
 import com.sj.study.web.dto.PostsResponseDto;
@@ -16,18 +17,20 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     // 머스테치 스타터 덕분에 컨트로로러에서 문자열을 반환할 때, 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
     // src/main/resources/templates
     // .mustache
     @GetMapping("/")
-    public String index(Model model) { // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+//    public String index(Model model) { // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+    public String index(Model model, @LoginUser SessionUser user) {
+        // 기존에(HttpSession 생성 및 httpSession.getAttribute("user")) 가져오던 값을 어느 컨트롤러든지 @LoginUser 만 사용하면 세션정보를 가져올 수 있게 설정
         model.addAttribute("posts", postsService.findAllDesc());
         /**CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 저장
          * 로그인 성공 시 httpSession.getAttribute("user") 값을 가져옴
          * */
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) { // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록, 값이 없으면(mustache 에서) 로그인 버튼이 보이게 됨
             model.addAttribute("userName", user.getName());
