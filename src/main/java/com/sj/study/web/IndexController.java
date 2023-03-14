66336856798c6b2b12ce507bcaa1fa7dda4a333e
Min-SungJo <123or@naver.com>
2023-03-14 13:36:45 +0900

    index.mustache 에 로그인 버튼을 추가, 로그인 성공 시 사용자 이름 노출, index.mustache 에서 userName 을 사용할 수 있게 IndexController 의 model 에 값 저장

--- src/main/java/com/sj/study/web/IndexController.java
+++ src/main/java/com/sj/study/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sj.study.web;
 
+import com.sj.study.config.auth.dto.SessionUser;
 import com.sj.study.service.posts.PostsService;
 import com.sj.study.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // 머스테치 스타터 덕분에 컨트로로러에서 문자열을 반환할 때, 앞의 경로와 뒤의 파일 확장자는 자동으로 지정됨
     // src/main/resources/templates
@@ -20,6 +24,15 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) { // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
         model.addAttribute("posts", postsService.findAllDesc());
+        /**CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 저장
+         * 로그인 성공 시 httpSession.getAttribute("user") 값을 가져옴
+         * */
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) { // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록, 값이 없으면(mustache 에서) 로그인 버튼이 보이게 됨
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index"; // src/main/resources/templates/index.mustache 로 전환되어 View Resolver 가  처리
     }
     @GetMapping("/posts/save")
